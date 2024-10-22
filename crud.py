f1fb4c6195c2bc3681adcb39086356c6f29f6aab
PopovicJovan <jovanp.school06@gmail.com>
2024-10-22 11:25:26 +0200

    routes for author model are added

--- crud.py
+++ crud.py
@@ -1,14 +1,15 @@
 from sqlalchemy.orm import Session
-from models import Quote
+from models import Quote, Author
 from fastapi import HTTPException
+from typing import Dict, Any
 
-
-def get_quotes(db: Session, author, page: int = 1, size: int = 6):
+# Quotes crud
+def get_quotes(db: Session, author_name: str = "", page: int = 1, size: int = 6) -> Dict[str, Any]:
     offset = (page-1)*size
 
     query = db.query(Quote)
-    if author:
-        query = query.filter(Quote.author_name.ilike(f'{author}%'))
+    if author_name:
+        query = query.join(Author).filter(Author.name.ilike(f'{author_name}%'))
 
     total_items = query.count()
     quotes = query.offset(offset).limit(size).all()
@@ -21,8 +22,8 @@ def get_quotes(db: Session, author, page: int = 1, size: int = 6):
     }
 
 
-def create_quote(db: Session, author: str, quote: str):
-    quote = Quote(author_name=author, quote=quote)
+def create_quote(db: Session, author_id: int, quote: str) -> Dict[str, Any]:
+    quote = Quote(author_id=author_id, quote=quote)
 
     db.add(quote)
     db.commit()
@@ -35,13 +36,57 @@ def create_quote(db: Session, author: str, quote: str):
     }
 
 
-def get_quote(db: Session, quote_id: int):
+def get_quote(db: Session, quote_id: int) -> Dict[str, Any] | HTTPException:
     quote = db.query(Quote).filter_by(id=quote_id).first()
     if not quote:
         return HTTPException(status_code=404, detail='Quote not found')
 
     return {
         'data': {
-            'quote': quote,
+            'quote': quote
+        }
+    }
+
+
+# Authors crud
+def get_authors(db: Session, author_name: str = "", page: int = 1, size: int = 6) -> Dict[str, Any]:
+    offset = (page-1)*size
+
+    query = db.query(Author)
+    if author_name:
+        query = query.filter(Author.name.ilike(f'{author_name}%'))
+
+    total_items = query.count()
+    authors = query.offset(offset).limit(size).all()
+
+    return {
+        'data': {
+            'author': authors,
+            'total_items': total_items
+        }
+    }
+
+def get_author(db: Session, author_id: int) -> Dict[str, Any] | HTTPException:
+    author = db.query(Author).filter_by(id=author_id).first()
+    if not author:
+        return HTTPException(status_code=404, detail='Author not found')
+
+    return {
+        'data': {
+            'author': author
+        }
+    }
+
+
+def create_author(db: Session, author_name: str) -> Dict[str, Any]:
+    author = Author(name=author_name)
+
+    db.add(author)
+    db.commit()
+    db.refresh(author)
+
+    return {
+        'data': {
+            'author': author,
         }
     }
--- main.py
+++ main.py
@@ -10,15 +10,30 @@ app = FastAPI()
 
 
 @app.post("/quotes")
-async def create_quote(author: str, quote: str, db: Session = Depends(get_db)):
-    return crud.create_quote(db=db, author=author, quote=quote)
+async def create_quote(author_id: int, quote: str, db: Session = Depends(get_db)):
+    return crud.create_quote(db=db, author_id=author_id, quote=quote)
 
 
 @app.get("/quotes")
-async def get_quotes(author: Optional[str] = None, db: Session = Depends(get_db)):
-    return crud.get_quotes(db=db, author=author)
+async def get_quotes(page: int, author_name: Optional[str] = None, db: Session = Depends(get_db)):
+    return crud.get_quotes(db=db, author_name=author_name, page=page)
 
 
 @app.get("/quotes/{quote_id}")
-async def get_quotes(quote_id: int, db: Session = Depends(get_db)):
+async def get_quote(quote_id: int, db: Session = Depends(get_db)):
     return crud.get_quote(db=db, quote_id=quote_id)
+
+
+@app.post("/authors")
+async def create_author(author_name: str, db: Session = Depends(get_db)):
+    return crud.create_author(db=db, author_name=author_name)
+
+
+@app.get("/authors")
+async def get_authors(page: int, author_name: Optional[str] = None, db: Session = Depends(get_db)):
+    return crud.get_authors(db=db, author_name=author_name, page=page)
+
+
+@app.get("/authors/{author_id}")
+async def get_author(author_id: int, db: Session = Depends(get_db)):
+    return crud.get_author(db=db, author_id=author_id)
